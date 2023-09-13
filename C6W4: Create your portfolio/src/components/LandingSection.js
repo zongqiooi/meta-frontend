8bd3f7e1ae587c34b2bf6585fda8cf5d2ad17341
zongqiooi <huro710@hotmail.com>
2023-09-13 21:21:54 +0800

    feat: add greeting and bio in landing section

--- C6W4: Create your portfolio/src/components/LandingSection.js
+++ C6W4: Create your portfolio/src/components/LandingSection.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import { Avatar, Heading, VStack, Box } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
 const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
-// Implement the UI for the LandingSection component according to the instructions.
-// Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
@@ -15,7 +13,18 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack>
+      <Avatar name="Pete" src="https://i.pravatar.cc/150?img=7" />
+      <Heading as="h5" size="sm" h="50px">
+        {greeting}
+      </Heading>
+      <Heading as="h2" size="xl">
+        {bio1}
+      </Heading>
+      <Heading as="h2" size="xl">
+        {bio2}
+      </Heading>
+    </VStack>
   </FullScreenSection>
 );
 
