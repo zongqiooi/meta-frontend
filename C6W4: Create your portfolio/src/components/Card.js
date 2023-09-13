8dc1ea7c387054f8f468c30ff78ce960c718080c
zongqiooi <huro710@hotmail.com>
2023-09-13 22:11:35 +0800

    feat: add cards UI in project section

--- C6W4: Create your portfolio/src/components/Card.js
+++ C6W4: Create your portfolio/src/components/Card.js
@@ -4,10 +4,26 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack
+      color="black"
+      backgroundColor="white"
+      curosr="pointer"
+      borderRadius="xl"
+    >
+      <Image src={imageSrc} alt={title} borderRadius="xl" />
+      <Heading as="h2" size="xl">
+        {title}
+      </Heading>
+      <Text fontSize="lg" color="light-grey">
+        {description}
+      </Text>
+      <HStack>
+        <Text fontSize="lg">See more</Text>
+        <FontAwesomeIcon icon={faArrowRight} size="1x" color="black" />
+      </HStack>
+    </VStack>
+  );
 };
 
 export default Card;
