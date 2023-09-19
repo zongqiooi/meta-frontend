a7b1d7655b5ca5d37831e459693a58b7e1b10d87
zongqiooi <huro710@hotmail.com>
2023-09-19 13:11:25 +0800

    feat: add nav bar section

--- C8 Capstone Project/src/App.jsx
+++ C8 Capstone Project/src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import Logo from "./assets/Logo.svg";
 
 function App() {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <header className="nav-flex-container">
+        <img src={Logo} alt="Little Lemon Restaurant Logo" />
+        <nav>
+          <ul className="links-flex-container">
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+          </ul>
+        </nav>
+      </header>
+
+      <main></main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
